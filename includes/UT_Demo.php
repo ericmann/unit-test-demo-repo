ac6ef8a5b12cff669234e65831b66730bf1328ff
Eric Mann <eric@eamann.com>
2013-04-02 15:03:32 -0700

    Update Documentation
    Update docblocks for class `UT_Demo` and introduce a caching bug that can be addressed.

--- includes/UT_Demo.php
+++ includes/UT_Demo.php
@@ -4,37 +4,34 @@
  */
 class UT_Demo {
 	/**
-	 * Grab a post from the database at random.
+	 * Grab a post from the database at random. Caches the post so that it
+	 * only refreshes once per day.
 	 *
 	 * @uses get_posts()
 	 *
 	 * @return object
 	 */
 	public function get_post_of_the_day() {
-		// Attempt to fetch the post of the day from a transient.
-		$post = get_transient( 'utdemo_post_of_the_day' );
+		$args = array(
+			'numberposts' => 1,      // Select only one post
+			'orderby'     => 'rand', // Select the post at random
+			'post_type'   => 'post'  // Only request regular posts
+		);
 
-		if ( false === $post ) {
-			$args = array(
-				'numberposts' => 1,      // Select only one post
-				'orderby'     => 'rand', // Select the post at random
-				'post_type'   => 'post'  // Only request regular posts
-			);
+		$posts = get_posts( $args );
 
-			$posts = get_posts( $args );
-
-			// Return the first post in the array
-			$post = $posts[0];
-
-			// Store the result in a transient for 24 hours so that we don't keep querying
-			set_transient( 'utdemo_post_of_the_day', $post, 60 * 60 * 24 );
-		}
-
-		return $post;
+		// Return the first post in the array
+		return $posts[0];
 	}
 	
 	/**
 	 * Trim an excerpt down to size.
+	 *
+	 * @uses strip_shortcodes()
+	 * @uses apply_filters()    Calls 'the_content'
+	 * @uses apply_filters()    Calls 'excerpt_length'
+	 * @uses apply_filters()    Calls 'excerpt_more'
+	 * @uses wp_trim_words()
 	 *
 	 * @param string $content
 	 *
@@ -46,7 +43,7 @@ class UT_Demo {
 		$text = apply_filters( 'the_content', $text );
 		$text = str_replace(']]>', ']]&gt;', $text);
 		$excerpt_length = apply_filters('excerpt_length', 55);
-		$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
+		$excerpt_more = apply_filters('excerpt_more', ' [...]');
 		$text = wp_trim_words( $text, $excerpt_length, $excerpt_more );
 
 		return $text;
